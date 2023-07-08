65625196eca51c44ab47134469e96a2d7f00cd39
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 14:32:26 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -8,5 +8,31 @@ public function onLoad(): void {
 $config = $this->getConfig();
   $this->saveDefaultConfig();
 }
-  
+public function chatScramble() {
+  $timer = $this->getConfig()->get("Timer"); 
+  // ^ time till end of scramble
+  $wordraw = $this->getConfig()->get("Words");
+  $word = $wordraw->str_shuffle();
+  $economy =  $this->getConfig()->get("MoneyRange");
+  $delay = $this->getConfig()->get("Delay");
+  // ^ time till next scramble
+  while($timer > 0){
+$this->getServer()->broadcastMessage("New Word to Scramble: " .  $word . "winner gets " . $economy)
+}
+  if ($timer === 0) {
+timer();
+    
+  }
+
+  }
+}
+  public function timer() {
+    while($delay > 0) {
+      return false;
+
+  }
+    if ($delay === 0) {
+chatScramble();
+    }
+  }
 }
