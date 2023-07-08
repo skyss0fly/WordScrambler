188e4a1bf30a47d35bfa8c83fbafde58e43e4f4d
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 15:53:23 +1000

    Update Main.php

--- src/skyss0fly/WordScrambler/Main.php
+++ src/skyss0fly/WordScrambler/Main.php
@@ -49,7 +49,7 @@ while($delay === 0 && $timer != 0) {
     return false;
   }
 $this->getServer()->broadcastMessage(onChat()->$player ."Unscrambled the word: " . chatScramble()->$wordraw . "and won " . chatScramble()->$economy)
-  chatScramble()->timer = 0
+  chatScramble()::timer = 0
   timer();
 }
 }
